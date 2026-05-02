7cd8d03c1792e5d7b450fd6779e7dea72a0f1414
Adalid Martinez <adalvarez2021@gmail.com>
2026-05-02 10:02:18 +0200

    feat: initialize Sanity Studio project configuration and dependencies

---
+++ hevca/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'HEVCA',
+
+  projectId: 'illa07bq',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
